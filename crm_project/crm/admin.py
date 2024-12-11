3dcd39342d8ed8d52a584413574f7ab68e9af91f
lvv-DA <vinubstat@gmail.com>
2024-12-11 14:02:40 +0000

    added api code

--- crm_project/crm/admin.py
+++ crm_project/crm/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, SalesRepresentative, Customer, Contact, Location, Opportunity, Client
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(SalesRepresentative)
+admin.site.register(Customer)
+admin.site.register(Contact)
+admin.site.register(Location)
+admin.site.register(Opportunity)
+admin.site.register(Client)
